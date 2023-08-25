17278509738680a1deca8c68f844798ea159705a
monikawojtek <134314429+monikawojtek@users.noreply.github.com>
2023-08-25 12:10:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,105 +1,182 @@
-//Przygotowywujemy sobie zmienną na planszę
-var tablica;
-var plansza;
-var ruch_numer;
-//Tworzymy zdarzenie które uruchomi sie jak strona zostanie załadowana
-window.addEventListener("load", function(){
-    //Po kliknięciu przycisku zaczynamy grę na nowo
-    document.getElementById("przycisk").addEventListener("click", function(){
-        document.getElementById("komunikat").setAttribute("hidden", "");
-        nowa_gra("x");
-    });
-    //Przypisujemy sobie nasza zmienną do planszy żabyśmy nie musieli ciągle jej wyszukiwać
-    plansza = document.getElementById("plansza");
-    //dodajemy do plansz zdarzenie wywoływane przez kliknięcie myszki
-    plansza.addEventListener("click", function(zdarzenie){
-        //Element zdarzenie.target zawiera wskaźnik na polę na którym nastąpiło kliknięcie
-        //Sprawdzamy czy user napewno klinką w pole a nie np. trafił na siatę, czy gdzieś obok.
-        if(zdarzenie.target.nodeName != "TH" || zdarzenie.target.hasAttribute("pole"))
-            return false;
-        zdarzenie.target.setAttribute("pole", plansza.getAttribute("ruch"));
-        var ruch = 1;
-        if(plansza.getAttribute("ruch") == "x")
-        {
-            ruch = -1;
-            plansza.setAttribute("ruch", "o");
-        }
-        else
-            plansza.setAttribute("ruch", "x");
-        //Dodaje kolejny ruch
-        ruch_numer++;
-        //Oznaczam pole w tablicy
-        tablica[parseInt(zdarzenie.target.getAttribute("x"))][parseInt(zdarzenie.target.getAttribute("y"))] = ruch;
-        switch(czy_koniec())
-        {
-            //najpopularniejsza opcja wiec na poczatek sprawdzamy i jak jest to przerywamy sprawdzanie
-            case false: break;
-            //Już wiemy że koniec więc blokujemy interfejs odbierając ruch
-            case -1:
-                wyswietl_komunikat("Wygrał krzyzyk");
-                break;
-            case 1:
-                wyswietl_komunikat("Wygrało kólko");
-                break;
-            case -2:
-                wyswietl_komunikat("Remis");
-                break;
-        }
-    });
-    nowa_gra("x");
-});
-//Funkcja do wyświetlania komunikatu na koniec gry
-function wyswietl_komunikat(tresc)
-{
-    document.getElementById("tresc").innerHTML = tresc;
-    plansza.removeAttribute("ruch");
-    document.getElementById("komunikat").removeAttribute("hidden");
-};
-//Podajemy kto zaczyna x - krzyzyk, o - kolko
-function nowa_gra(zaczyna)
-{
-    //Resetuje liczbę ruchów
-    ruch_numer = 0;
-    //Tworzymy tabelę jednowymiarową
-    tablica = new Array(3);
-    //Dodajemy drugi wymiar tworząc w każdym wierszi tabeli kolejną tabelę
-    for(var i = 0; i < tablica.length;i++)
-    {
-        tablica[i] = new Array(tablica.length);
-        for(var x = 0; x < tablica.length; x++)
-        tablica[i][x] = 0;
-    }
-    //Sprawdzamy czy są atrybuty pola, jesli tak to je usuwamy 
-    var zapelnione_pola = plansza.querySelectorAll("th[pole]");
-    for(var i = 0; i < zapelnione_pola.length; i++)
-        zapelnione_pola[i].removeAttribute("pole");
-    plansza.setAttribute("ruch", zaczyna);
-};
-function czy_koniec()
-{
-    /*
-        -1 - krzyzk
-        1 - kolko,
-        -2 - remis
-        false/0 - jeszcze nie koniec
-    */
-    //Sprawdzam czy nie ma wygranej po prostej
-    for(var i = 0; i < 3; i++)
-    {
-        //Sprawdzam w poziomie
-        if(Math.abs(tablica[0][i] + tablica[1][i] + tablica[2][i]) == 3)
-            return tablica[0][i];
-        //Sprawdzam w pionie
-        if(Math.abs(tablica[i][0] + tablica[i][1] + tablica[i][2]) == 3)
-            return tablica[i][0];
-    }
-    //Sprawdzam skosy
-    if(Math.abs(tablica[0][0] + tablica[1][1] + tablica[2][2]) == 3)
-        return tablica[0][0];
-    if(Math.abs(tablica[0][2] + tablica[1][1] + tablica[2][0]) == 3)
-        return tablica[0][2];
-    //Sprawdzam czy plansza nie jest zapelniona jak jest i nie ma nadal wygranej to remis
-    if(ruch_numer >= 9)
-        return -2;
-   return false;
-}
+
+
+
+(function(){
+	
+	var Memory = {
+
+		init: function(cards){
+			this.$game = $(".game");
+			this.$modal = $(".modal");
+			this.$overlay = $(".modal-overlay");
+			this.$restartButton = $("button.restart");
+			this.cardsArray = $.merge(cards, cards);
+			this.shuffleCards(this.cardsArray);
+			this.setup();
+		},
+
+		shuffleCards: function(cardsArray){
+			this.$cards = $(this.shuffle(this.cardsArray));
+		},
+
+		setup: function(){
+			this.html = this.buildHTML();
+			this.$game.html(this.html);
+			this.$memoryCards = $(".card");
+			this.paused = false;
+     	this.guess = null;
+			this.binding();
+		},
+
+		binding: function(){
+			this.$memoryCards.on("click", this.cardClicked);
+			this.$restartButton.on("click", $.proxy(this.reset, this));
+		},
+		// kinda messy but hey
+		cardClicked: function(){
+			var _ = Memory;
+			var $card = $(this);
+			if(!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")){
+				$card.find(".inside").addClass("picked");
+				if(!_.guess){
+					_.guess = $(this).attr("data-id");
+				} else if(_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")){
+					$(".picked").addClass("matched");
+					_.guess = null;
+				} else {
+					_.guess = null;
+					_.paused = true;
+					setTimeout(function(){
+						$(".picked").removeClass("picked");
+						Memory.paused = false;
+					}, 600);
+				}
+				if($(".matched").length == $(".card").length){
+					_.win();
+				}
+			}
+		},
+
+		win: function(){
+			this.paused = true;
+			setTimeout(function(){
+				Memory.showModal();
+				Memory.$game.fadeOut();
+			}, 1000);
+		},
+
+		showModal: function(){
+			this.$overlay.show();
+			this.$modal.fadeIn("slow");
+		},
+
+		hideModal: function(){
+			this.$overlay.hide();
+			this.$modal.hide();
+		},
+
+		reset: function(){
+			this.hideModal();
+			this.shuffleCards(this.cardsArray);
+			this.setup();
+			this.$game.show("slow");
+		},
+
+		// Fisher--Yates Algorithm -- https://bost.ocks.org/mike/shuffle/
+		shuffle: function(array){
+			var counter = array.length, temp, index;
+	   	// While there are elements in the array
+	   	while (counter > 0) {
+        	// Pick a random index
+        	index = Math.floor(Math.random() * counter);
+        	// Decrease counter by 1
+        	counter--;
+        	// And swap the last element with it
+        	temp = array[counter];
+        	array[counter] = array[index];
+        	array[index] = temp;
+	    	}
+	    	return array;
+		},
+
+		buildHTML: function(){
+			var frag = '';
+			this.$cards.each(function(k, v){
+				frag += '<div class="card" data-id="'+ v.id +'"><div class="inside">\
+				<div class="front"><img src="'+ v.img +'"\
+				alt="'+ v.name +'" /></div>\
+				<div class="back"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/codepen-logo.png"\
+				alt="Codepen" /></div></div>\
+				</div>';
+			});
+			return frag;
+		}
+	};
+
+	var cards = [
+		{
+			name: "php",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/php-logo_1.png",
+			id: 1,
+		},
+		{
+			name: "css3",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/css3-logo.png",
+			id: 2
+		},
+		{
+			name: "html5",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/html5-logo.png",
+			id: 3
+		},
+		{
+			name: "jquery",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/jquery-logo.png",
+			id: 4
+		}, 
+		{
+			name: "javascript",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/js-logo.png",
+			id: 5
+		},
+		{
+			name: "node",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/nodejs-logo.png",
+			id: 6
+		},
+		{
+			name: "photoshop",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/photoshop-logo.png",
+			id: 7
+		},
+		{
+			name: "python",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/python-logo.png",
+			id: 8
+		},
+		{
+			name: "rails",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/rails-logo.png",
+			id: 9
+		},
+		{
+			name: "sass",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sass-logo.png",
+			id: 10
+		},
+		{
+			name: "sublime",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sublime-logo.png",
+			id: 11
+		},
+		{
+			name: "wordpress",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/wordpress-logo.png",
+			id: 12
+		},
+	];
+    
+	Memory.init(cards);
+
+
+})();
